ce588be52e6b774049bd9fdabf65a0746936c08a
amongash <amongash08@gmaill.com>
2016-06-02 10:15:05 +0300

    Improved datatables

--- application/modules/stock/models/Mdl_stock.php
+++ application/modules/stock/models/Mdl_stock.php
@@ -6,7 +6,6 @@ use
     DataTables\Editor\Join,
     DataTables\Editor\Upload,
     DataTables\Editor\Validate;
-     
 
 class Mdl_Stock extends CI_Model
 {
@@ -48,7 +47,7 @@ class Mdl_Stock extends CI_Model
             ) )
             ->getFormatter( 'Format::date_sql_to_format', Format::DATE_ISO_8601 )
             ->setFormatter( 'Format::date_format_to_sql', Format::DATE_ISO_8601 )
-        )
+        )->selectDistinct('v_transactions_all', '*')
         ->where( 'station', $station )
         ->where( 'vaccine_id', $vaccine_id )
         ->process( $post )
