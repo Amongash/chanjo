9ef14363370faf498a325e6247f85dcb3e287542
Mureithi <kelvinmwas@gmail.com>
2016-06-02 16:47:03 +0300

    dashboard part 3

--- application/modules/dashboard/controllers/Dashboard.php
+++ application/modules/dashboard/controllers/Dashboard.php
@@ -50,15 +50,15 @@ Modules::run('secure_tings/is_logged_in');
             $data['best'] = $this->best_dpt3cov($station_id, $option);
             $data['worst'] = $this->worst_dpt3cov($station_id, $option);
           } else {
-            //$option = '4';
-            //$data['best'] = $this->best_dpt3cov($station_id, $option);
-          //  $data['worst'] = $this->worst_dpt3cov($station_id, $option);
+            $option = '4';
+            $data['best'] = $this->best_dpt3cov($station_id, $option);
+            $data['worst'] = $this->worst_dpt3cov($station_id, $option);
           }
           //echo '<pre>',print_r($data),'</pre>';exit;
 
 
         $data['section'] = "NVIP-Chanjo";
-        $data['view_file'] = "dashboard_view";
+        $data['view_file'] = "dashboard_new";
         $data['module'] = "dashboard";
         $data['loc'] = $user_level;
 
@@ -79,12 +79,13 @@ Modules::run('secure_tings/is_logged_in');
 
         if ($option == '1') {
            $query = $this->mdl_dashboard->best_region_dpt3();
+           //echo '<pre>',print_r($query),'</pre>';exit;
         } elseif ($option == '2') {
             $query = $this->mdl_dashboard->best_county_dpt3($station_id);
         } elseif ($option == '3') {
              $query = $this->mdl_dashboard->best_subcounty_dpt3($station_id);
         } elseif ($option == '4') {
-             //$query = $this->mdl_dashboard->best_facility_dpt3($station_id);
+             $query = $this->mdl_dashboard->best_facility_dpt3($station_id);
              //echo '<pre>',print_r(json_encode($station_id),true),'</pre>';exit;
         }
 
@@ -136,64 +137,7 @@ Modules::run('secure_tings/is_logged_in');
     }
 
 
-   function positiveColdchain(){
-
-     $info['user_object'] = $this->get_user_object();
-     if (isset($_GET['loc']) ) {
-         if (!empty($_GET['loc'])) {
-           $user_level = (int)$_GET['loc'];
-           $user_station = (int)$_GET['name'];
-           }
-         }else{
-            $user_level = $info['user_object']['user_level'];
-            $user_station = $info['user_object']['user_statiton'];
-         }
-
-     if ($user_level == '1') {
-       return $this->positivecoldchainNational();
-     }elseif ($user_level == '2') {
-       return $this->positivecoldchainRegion();
-     }elseif ($user_level == '3') {
-       # code...
-     }
-     elseif ($user_level == '4') {
-       # code...
-     }else {
-       return 'Error';
-     }
-
-
-   }
-
-   function negativeColdchain(){
-
-     $info['user_object'] = $this->get_user_object();
-     if (isset($_GET['loc']) ) {
-         if (!empty($_GET['loc'])) {
-           $user_level = (int)$_GET['loc'];
-           $user_station = (int)$_GET['name'];
-           }
-         }else{
-            $user_level = $info['user_object']['user_level'];
-            $user_station = $info['user_object']['user_statiton'];
-         }
-
-     if ($user_level == '1') {
-       return $this->negativecoldchainNational();
-     }elseif ($user_level == '2') {
-       return $this->negativecoldchainRegion();
-     }elseif ($user_level == '3') {
-       # code...
-     }
-     elseif ($user_level == '4') {
-       # code...
-     }else {
-       return 'Error';
-     }
-
-   }
-
-   protected function _station($var){
+  protected function _station($var){
      $station = str_replace('%20', ' ', $var);
      return $station;
    }
@@ -314,13 +258,53 @@ Modules::run('secure_tings/is_logged_in');
      $this -> load -> view("dashboard",$data);
    }
 
-   function positivecoldchainNational(){
+   function positivecoldchain($escalate,$station){
+
+
+     $station=str_replace('%20',' ',$station);
+
      $info['user_object'] = $this->get_user_object();
-     $user_level = $info['user_object']['user_level'];
+
+     if ($escalate =='NULL' && $station =='NULL') {
+       $data['user_level']  = $info['user_object']['user_level'];
+       $station = $info['user_object']['user_statiton'];
+       $escalate= $info['user_object']['user_level'];
+     }else {
+
+       $data['user_level']  = $escalate;
+       $station = $station;
+     }
+
      $this->load->model('mdl_dashboard');
-     $query_total = $this->mdl_dashboard->get_vaccine_volume_national();
-     $query_opv = $this->mdl_dashboard->get_opv_vaccine_volume_national();
-     $total_capacity = $this->mdl_dashboard->get_fridge_cold_chain_capacity_national();
+
+     if ($escalate=='1') {
+
+       $query_total = $this->mdl_dashboard->get_vaccine_volume_national($station);
+       $query_opv = $this->mdl_dashboard->get_opv_vaccine_volume_national($station);
+       $total_capacity = $this->mdl_dashboard->get_fridge_cold_chain_capacity_national($station);
+
+     }elseif ($escalate == '2') {
+
+       $query_total = $this->mdl_dashboard->get_vaccine_volume_national($station);
+       $query_opv = $this->mdl_dashboard->get_opv_vaccine_volume_national($station);
+       $total_capacity = $this->mdl_dashboard->get_fridge_cold_chain_capacity_national($station);
+
+     }elseif ($escalate == '3') {
+
+       $query_total = $this->mdl_dashboard->get_vaccine_volume_national($station);
+       $query_opv = $this->mdl_dashboard->get_opv_vaccine_volume_national($station);
+       $total_capacity = $this->mdl_dashboard->get_fridge_cold_chain_capacity_national($station);
+     }elseif ($escalate == '4') {
+
+       $query_total = $this->mdl_dashboard->get_vaccine_volume_national($station);
+       $query_opv = $this->mdl_dashboard->get_opv_vaccine_volume_national($station);
+       $total_capacity = $this->mdl_dashboard->get_fridge_cold_chain_capacity_national($station);
+    }else {
+      return coverageFacility($station);
+    }
+    //echo '<pre>',print_r($query_total),'</pre>';exit;
+
+
      $query_total=json_decode(json_encode($query_total),true);
      $query_opv=json_decode(json_encode($query_opv),true);
      $total_capacity=json_decode(json_encode($total_capacity),true);
@@ -328,18 +312,32 @@ Modules::run('secure_tings/is_logged_in');
      $opv_volume=$query_opv[0]['volume'];
      $unusedcapacity=$total_capacity[0]['total_volume']-$positivecoldchain;
 
-     $data['graph_title'] = "+ve Cold Chain Capacity, 2016 to May";
+     $data['graph_title'] = "+ve Cold Chain Capacity";
      $data['graph_id'] = "positive";
      $data['legend'] = "Litres";
      $data['colors'] = "['#008080','#6AF9C4']";
-     $data['piedata'] = json_encode((int)$positivecoldchain);
-     $data['remaining_volume'] = json_encode((int)$unusedcapacity);
+     $data['piedata'] = json_encode((float)$positivecoldchain);
+     $data['remaining_volume'] = json_encode((float)$unusedcapacity);
      $this -> load -> view("pie_template",$data);
    }
 
-   function negativecoldchainNational(){
+   function negativecoldchain($escalate,$station){
+
+     $station=str_replace('%20',' ',$station);
+
      $info['user_object'] = $this->get_user_object();
-     $user_level = $info['user_object']['user_level'];
+
+     if ($escalate =='NULL' && $station =='NULL') {
+       $data['user_level']  = $info['user_object']['user_level'];
+       $station = $info['user_object']['user_statiton'];
+       $escalate= $info['user_object']['user_level'];
+     }else {
+
+       $data['user_level']  = $escalate;
+       $station = $station;
+     }
+
+
      $this->load->model('mdl_dashboard');
      $query_opv = $this->mdl_dashboard->get_opv_vaccine_volume_national();
      $total_capacity = $this->mdl_dashboard->get_freezer_cold_chain_capacity_national();
@@ -352,12 +350,12 @@ Modules::run('secure_tings/is_logged_in');
      $data['graph_id'] = "negative";
      $data['legend'] = "Litres";
      $data['colors'] = "['#008080','#6AF9C4']";
-     $data['remaining_volume'] = json_encode((int)$unusedcapacity);
-     $data['piedata'] = json_encode((int)$opv_volume);
+     $data['remaining_volume'] = json_encode((float)$unusedcapacity);
+     $data['piedata'] = json_encode((float)$opv_volume);
      $this -> load -> view("pie_template",$data);
    }
 
-   function coverage($station='NULL'){
+   function coverage($station='NULL',$escalate='NULL'){
      $info['user_object'] = $this->get_user_object();
      $user_level = $info['user_object']['user_level'];
      $this->load->model('mdl_dashboard');
@@ -366,34 +364,40 @@ Modules::run('secure_tings/is_logged_in');
      $interval = new DateInterval('P12M');
      $mindate=$mindate->sub($interval)->format('Y-m-d');
 
-
-     if ($station=='NULL') {
+     if ($escalate =='NULL' && $station =='NULL') {
+       $data['user_level']  = $info['user_object']['user_level'];
        $station = $info['user_object']['user_statiton'];
+       $escalate= $info['user_object']['user_level'];
+     }else {
+
+       $data['user_level']  = $escalate;
+       $station_id = $station;
      }
+
+
      $station=str_replace('%20',' ',$station);
 
      $this->load->model('mdl_dashboard');
 
 
-
-     if ($user_level=='1') {
+     if ($escalate=='1') {
 
        $query = $this->mdl_dashboard->get_national_coverage($maxdate,$mindate);
 
-     }elseif ($user_level == '2') {
+     }elseif ($escalate == '2') {
 
        $query = $this->mdl_dashboard->get_region_coverage($maxdate,$mindate,$station);
-     }elseif ($user_level == '3') {
+     }elseif ($escalate == '3') {
 
-       $query = $this->mdl_dashboard->get_national_coverage($maxdate,$mindate,$station);
-     }elseif ($user_level == '4') {
+       $query = $this->mdl_dashboard->get_county_coverage($maxdate,$mindate,$station);
+     }elseif ($escalate == '4') {
 
-      $query = $this->mdl_dashboard->get_national_coverage($maxdate,$mindate,$station);
+      $query = $this->mdl_dashboard->get_subcounty_coverage($maxdate,$mindate,$station);
     }else {
       return coverageFacility($station);
     }
 
-  
+
 
       $query=json_decode(json_encode($query),true);
 
@@ -407,21 +411,21 @@ Modules::run('secure_tings/is_logged_in');
 
      foreach ($query as $key =>$value ) {
        $time_data[]=$value['months'];
-       $bcg[]=(int)$value['bcg']*12;
-       $dpt1[]=(int)$value['dpt1']*12;
-       $dpt2[]=(int)$value['dpt2']*12;
-       $dpt3[]=(int)$value['dpt3']*12;
-       $measles1[]=(int)$value['measles1']*12;
-       $measles2[]=(int)$value['measles2']*12;
-       $measles3[]=(int)$value['measles3']*12;
-       $opv1[]=(int)$value['opv1']*12;
-       $opv2[]=(int)$value['opv2']*12;
-       $opv3[]=(int)$value['opv3']*12;
-       $pvc1[]=(int)$value['pcv1']*12;
-       $pvc2[]=(int)$value['pcv2']*12;
-       $pvc3[]=(int)$value['pcv3']*12;
-       $rota1[]=(int)$value['rota1']*12;
-       $rota2[]=(int)$value['rota2']*12;
+       $bcg[]=(float)$value['bcg']*12;
+       $dpt1[]=(float)$value['dpt1']*12;
+       $dpt2[]=(float)$value['dpt2']*12;
+       $dpt3[]=(float)$value['dpt3']*12;
+       $measles1[]=(float)$value['measles1']*12;
+       $measles2[]=(float)$value['measles2']*12;
+       $measles3[]=(float)$value['measles3']*12;
+       $opv1[]=(float)$value['opv1']*12;
+       $opv2[]=(float)$value['opv2']*12;
+       $opv3[]=(float)$value['opv3']*12;
+       $pvc1[]=(float)$value['pcv1']*12;
+       $pvc2[]=(float)$value['pcv2']*12;
+       $pvc3[]=(float)$value['pcv3']*12;
+       $rota1[]=(float)$value['rota1']*12;
+       $rota2[]=(float)$value['rota2']*12;
 
      }
      $data['graph_title'] = "Coverage";
@@ -555,10 +559,84 @@ Modules::run('secure_tings/is_logged_in');
 
       $this -> load -> view("facility_coverage",$data);
 
-     //
+    }
+
+     function best($escalate,$name){
+       $name=str_replace('%20',' ',$name);
+
+       $info['user_object'] = $this->get_user_object();
+
+       if ($escalate =='NULL' && $name =='NULL') {
+         $data['user_level']  = $info['user_object']['user_level'];
+         $station_id = $info['user_object']['user_statiton'];
+         $escalate= $info['user_object']['user_level'];
+       }else {
+
+         $data['user_level']  = $escalate;
+         $station_id = $name;
+       }
+
+      // echo '<pre>',print_r($station_id),'</pre>';exit;
 
 
-   }
+       if ($escalate == '1') {
+         $option = '1';
+         $data['best'] = $this->best_dpt3cov($station_id, $option);
+
+       } elseif ($escalate == '2') {
+         $option = '2';
+         $data['best'] = $this->best_dpt3cov($station_id, $option);
+       } elseif ($escalate == '3') {
+         $option = '3';
+         $data['best'] = $this->best_dpt3cov($station_id, $option);
+       } else {
+         $option = '4';
+         $data['best'] = $this->best_dpt3cov($station_id, $option);
+       }
+
+
+       $this -> load -> view("coverage_performance",$data);
+
+
+     }
+
+     function worst($escalate,$name){
+
+       $name=str_replace('%20',' ',$name);
+
+       $info['user_object'] = $this->get_user_object();
+
+       if ($escalate =='NULL' && $name =='NULL') {
+         $data['user_level']  = $info['user_object']['user_level'];
+         $station_id = $info['user_object']['user_statiton'];
+         $escalate= $info['user_object']['user_level'];
+       }else {
+
+         $data['user_level']  = $escalate;
+         $station_id = $name;
+       }
+
+
+       if ($escalate == '1') {
+         $option = '1';
+         $data['worst'] = $this->worst_dpt3cov($station_id, $option);
+
+       } elseif ($escalate == '2') {
+         $option = '2';
+         $data['worst'] = $this->worst_dpt3cov($station_id, $option);
+       } elseif ($escalate == '3') {
+         $option = '3';
+         $data['worst'] = $this->worst_dpt3cov($station_id, $option);
+       } else {
+         $option = '4';
+         $data['worst'] = $this->worst_dpt3cov($station_id, $option);
+       }
+
+
+       $this -> load -> view("coverage_worst",$data);
+
+
+     }
 
 
 
